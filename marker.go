2adc5253f36a1b89740f00df5b7bed2596dcfd4f
evandigby <evandigby@gmail.com>
2018-07-02 18:18:54 -0700

    added marker events

--- marker.go
+++ marker.go
@@ -1,9 +1,11 @@
 package leaflet
 
-func NewMarker(c *Coordinate) *Marker {
+func NewMarker(c *Coordinate, events Events) *Marker {
+	m := gL.Call("marker", c.Value)
+	events.Bind(m)
 	return &Marker{
 		Layer: Layer{
-			Value: gL.Call("marker", c.Value),
+			Value: m,
 		},
 	}
 }
